b5b43314ac377c9a935061d7145dea3bfdc1138b
abduromanov2020 <abduromanov2020@gmail.com>
2023-01-03 14:47:05 +0700

    initial commits

---
+++ src/App.js
@@ -0,0 +1,144 @@
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  Text,
+  SkeletonText,
+} from "@chakra-ui/react";
+import { FaLocationArrow, FaTimes } from "react-icons/fa";
+
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+import { useRef, useState } from "react";
+
+const center = { lat: -6.352934306539937, lng: 106.83248006215435 };
+
+function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
+  });
+
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState(null);
+  const [duration, setDuration] = useState(null);
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef();
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destionationRef = useRef();
+
+  if (!isLoaded) {
+    return <SkeletonText noOfLines={4} spacing="4" />;
+  }
+
+  async function calculateRoute() {
+    if (!originRef.current.value || !destionationRef.current.value) {
+      return;
+    }
+
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destionationRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+
+    setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
+  }
+
+  function clearRoute() {
+    setDirectionsResponse(null);
+    setDistance(null);
+    setDuration(null);
+    originRef.current.value = "";
+    destionationRef.current.value = "";
+  }
+
+  return (
+    <Flex
+      position="relative"
+      flexDirection="column"
+      alignItems="center"
+      h="100vh"
+      w="100vw"
+    >
+      <Box position="absolute" left={0} top={0} h="100%" w="100%">
+        {/* {Google Map Box} */}
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          // prettier-ignore
+
+          onLoad={map => setMap(map)}
+        >
+          <Marker position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        </GoogleMap>
+      </Box>
+
+      <Box
+        p={4}
+        borderRadius="lg"
+        mt={4}
+        bgColor="white"
+        shadow="base"
+        minW="container.md"
+        zIndex="1"
+      >
+        <HStack spacing={4}>
+          <Autocomplete>
+            <Input type="text" placeholder="Origin" ref={originRef} />
+          </Autocomplete>
+
+          <Autocomplete>
+            <Input
+              type="text"
+              placeholder="Destination"
+              ref={destionationRef}
+            />
+          </Autocomplete>
+          <ButtonGroup>
+            <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
+              Calculate Route
+            </Button>
+            <IconButton
+              aria-label="center back"
+              icon={<FaTimes />}
+              onClick={clearRoute}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent="space-between">
+          <Text>Distance: {distance} </Text>
+          <Text>Duration: {duration} </Text>
+          <IconButton
+            aria-label="center back"
+            icon={<FaLocationArrow />}
+            isRound
+            onClick={() => map.panTo(center)}
+          />
+        </HStack>
+      </Box>
+    </Flex>
+  );
+}
+
+export default App;
